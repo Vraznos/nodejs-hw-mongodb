36401af74ff3ea00a6e369518af205cebdca1fcb
Vladyslav Voronin <157734203+Vraznos@users.noreply.github.com>
2024-11-18 18:38:07 +0100

    hw5-mistakes

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import contactsRouter from './routes/contacts.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import errorHandler from './middlewares/errorHandler.js';
 import logger from './middlewares/logger.js';
+import authRouter from './routes/auth.js';
+import cookieParser from 'cookie-parser';
 
 export const setupServer = () => {
   const app = express();
@@ -12,6 +14,8 @@ export const setupServer = () => {
   app.use(logger);
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
+  app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use(notFoundHandler);
   app.use(errorHandler);
